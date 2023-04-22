1fa62201612f059cac3021407634db980d2d255b
LeTemp2806 <tabaco225@hotmail.com>
2023-04-22 00:00:41 -0500

    Paso: Clonar en pc

--- api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -1,7 +1,20 @@
 package com.parking.apiparking.controller;
 
-public class ParkingController {
+import com.parking.apiparking.services.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-    
+@RestController
+@RequestMapping("/parking") //parking => url
+public class ParkingController {
+    //Inyección de dependencias => instancia => new ParkingService()
+    public ParkingController(ParkingService parkingService){
+        this.parkingService = parkingService;
+    }
+    @GetMapping("/cars")
+    public List<Car> getAllCars(){
+        
+    }
 
 }
